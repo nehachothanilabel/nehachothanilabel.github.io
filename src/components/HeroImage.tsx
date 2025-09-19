4cba00c8d9f0e19ecb2ed18fb6fbc88b2feaed35
Neha Chothani <nehachothanilabel@gmail.com>
2025-09-19 09:29:20 -0700

    change mobile img

--- src/components/HeroImage.tsx
+++ src/components/HeroImage.tsx
@@ -4,13 +4,38 @@ import CardActionArea from "@mui/material/CardActionArea";
 import CardMedia from "@mui/material/CardMedia";
 import { useNavigate } from "react-router-dom";
 import heroImage from "../images/event.jpg";
+import heroImageMobile from "../images/event_m.jpg";
 
 export default function HeroImage() {
   const navigate = useNavigate();
   return (
-    <Box sx={{ mx: { xs: 2, sm: 4 }, mt: { xs: 2, sm: 4 } }}>
+<>
+    <Box
+      sx={{
+        display: {
+          md: "none",
+        },
+      }}
+    >
+       <Card sx={{ maxWidth: 2000 }}>
+        <CardActionArea>
+          <CardMedia
+            sx={{ height: { xs: 560 } }}
+            image={heroImageMobile}
+            title="Hero image"
+          />
+        </CardActionArea>
+      </Card> 
+    </Box>
+    <Box
+      sx={{
+        display: {
+          xs: "none",
+          md: "block",
+        },
+      }}
+    >
       <Card sx={{ maxWidth: 2000 }}>
-        {/* <CardActionArea onClick={() => navigate("/product/w9")}> */}
         <CardActionArea>
           <CardMedia
             sx={{ height: { xs: 230, sm: 800 } }}
@@ -20,5 +45,18 @@ export default function HeroImage() {
         </CardActionArea>
       </Card>
     </Box>
+    {/* <Box sx={{ mx: { xs: 2, sm: 4 }, mt: { xs: 2, sm: 4 } }}>
+      <Card sx={{ maxWidth: 2000 }}>
+        {/* <CardActionArea onClick={() => navigate("/product/w9")}> 
+        <CardActionArea>
+          <CardMedia
+            sx={{ height: { xs: 230, sm: 800 } }}
+            image={heroImage}
+            title="Hero image"
+          />
+        </CardActionArea>
+      </Card>
+    </Box> */}
+    </>
   );
 }
